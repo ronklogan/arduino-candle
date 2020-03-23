7545a547b269511818ce1f94bd99fad30cfe0df2
Ron Logan <ronklogan@hotmail.com>
2020-03-23 07:16:43 -1000

    Add license text to .INO file

--- candle.ino
+++ candle.ino
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2020 Ron Logan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <Adafruit_NeoPixel.h>
 
 // output pin to which the neopixel strip input is attached
@@ -20,8 +44,8 @@
 //   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
 //   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(
-  NUMBER_OF_PIXELS, 
-  PIN_OUT, 
+  NUMBER_OF_PIXELS,
+  PIN_OUT,
   NEO_GRB + NEO_KHZ800);
 
 uint8_t lowPass(uint8_t current, uint8_t target) {
@@ -53,7 +77,7 @@ uint32_t randomColor() {
 void reset() {
   for(int pixelIndex = 0; pixelIndex < NUMBER_OF_PIXELS; ++pixelIndex) {
     strip.setPixelColor(pixelIndex, randomColor());
-  }  
+  }
 }
 
 // just a structure to get at the individual bytes of a 32-bit value
